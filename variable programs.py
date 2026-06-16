19bb7bcb29b3869da3d47f1827b022eabd60941c
poojitha-9933 <cnpoojitha301@gmail.com>
2026-06-16 22:50:08 +0530

    Add files via upload

---
+++ variable programs.py
@@ -0,0 +1,67 @@
+print('~~1st prgm~~')
+name='poojitha'
+age=19
+print('Name:',name)
+print('Age:',age)
+
+
+a=10
+b=20
+sum=a+b
+print('~~2nd prgm~~')
+print('sum =',sum)
+
+a=5
+b=10
+a,b=b,a
+print('~~3rs prgm~~')
+print("a=",a)
+print("b=",b)
+
+length=10
+breadth=5
+area=length*breadth
+print('~~4th prgm~~')
+print('area=',area)
+
+p=1000
+r=5
+t=2
+si=(p*r*t)/100
+print('~~5th prgm~~')
+print('Simple Intrest=',si)
+
+
+student_name='ravi'
+marks = 85
+print('~~6th prgm~~')
+print('student:',student_name)
+print('marks:',marks)
+
+a=10
+b=20
+c=30
+average=(a+b+c)/3
+print('~~7th prgm~~')
+print('average=',average)
+
+
+num=5
+square=num*num
+cube=num*num*num
+print('~~8th prgm~~')
+print('Square=',square)
+print('Cube=',cube)
+
+
+celsius=25
+fahrenheit=(celsius*9/5)+32
+print('~~9th prgm~~')
+print('Fahrenheit=',fahrenheit)
+
+
+price=100
+quantity=4
+total=price*quantity
+print('~~10th prgm~~')
+print('Total price=',total)
